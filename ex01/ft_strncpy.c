e60c9a35f3f47e9901937f0055fb24df9ec07a3f
Your Name <you@example.co>
2020-02-12 15:30:40 +0100

    gros fils de pute

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,9 +6,19 @@
 /*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/09 10:18:10 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/02/11 16:47:07 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/02/12 15:19:36 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int		ft_sizeof(char *tab)
+{
+	int i;
+
+	i = -1;
+	while (tab[++i])
+		NULL;
+	return (i);
+}
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
@@ -24,6 +34,7 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	}
 	i--;
 	while (++i <= n)
-		dest[i] = 0;
+		if (ft_sizeof(src) <= n)
+			dest[i] = 0;
 	return (dest);
 }
